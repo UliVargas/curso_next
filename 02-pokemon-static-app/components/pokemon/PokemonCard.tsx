33a6160b346c85d6568e4d86ed9b896e25666bcc
Ulises Vargas <uli.Vargas02@gmail.com>
2022-04-08 18:15:20 -0500

    Quito componente de Row

--- 02-pokemon-static-app/components/pokemon/PokemonCard.tsx
+++ 02-pokemon-static-app/components/pokemon/PokemonCard.tsx
@@ -30,15 +30,13 @@ export const PokemonCard: FC<Props> = ({pokemon}) => {
             width={'100%'}
             height={140}
           />
-          <Card.Footer>
-            <Row justify={'space-between'}>
+          <Card.Footer css={{ display: 'flex', justifyContent: 'space-between' }}>
               <Text transform={'capitalize'}>
                 {pokemon.name}
               </Text>
               <Text>
                 #{pokemon.id}
               </Text>
-            </Row>
           </Card.Footer>
         </Card.Body>
       </Card>
